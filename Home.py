a6bbb6def9a4609310d3bd735ed67fc5b9533f5e
amaxj95 <amaxj95@gmail.com>
2024-03-23 18:19:23 -0700

    init

--- Home.py
+++ Home.py
@@ -9,8 +9,10 @@ st.markdown("<h1 style='text-align: center;'>Welcome to the Junk-EZ Co.'s Online
 st.markdown("<h2 style='text-align: center;'>The easy Upcycling & Hauling System</h2>", unsafe_allow_html=True)
 
 # Centered content
-st.markdown("<p style='text-align: center;'>We specialize in Curbside trash Valet and Junk Removal. At Junk-EZ we make hauling junk and getting "
-            "debris removed EZ. For now, our drivers take your items to a local landfill. As we grow we look forward "
-            "revolutionizing our waste management system. We understand that no one wants to handle trash. So we’ve created a "
-            "patent-pending system that allows for all users to make money by responsibly recycling. To get started, navigate to the Junk&Debris or Curbside"
-            "collection pages to get started</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center;'>We specialize in Curbside trash Valet and Junk Removal. At Junk-EZ we make"
+            "hauling junk and getting debris removed EZ. For now, our drivers take your items to a local landfill. As "
+            "we"
+            "grow we look forward revolutionizing our waste management system. We understand that no one wants to "
+            "handle"
+            "trash. So we’ve created a patent-pending system that allows for all users to make money by responsibly"
+            "recycling. To get started, navigate to the Junk&Debris or Curbside collection pages to get started</p>", unsafe_allow_html=True)
